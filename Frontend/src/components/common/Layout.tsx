48f8c0a200dc2afef4340076a7561fbb8f4b0f61
Uditha Balalla <Udithabalalla@users.noreply.github.com>
2026-04-27 17:17:16 +0530

    Fix main layout className

--- Frontend/src/components/common/Layout.tsx
+++ Frontend/src/components/common/Layout.tsx
@@ -9,7 +9,7 @@ const Layout = () => {
       <Sidebar />
       <SidebarInset>
         <Header />
-        <main className="flex-1 p-4 md:p-8 max-w-[1600px] mx-auto animate-fade-in-up">
+        <main className="flex-1 p-4 md:p-8 animate-fade-in-up">
           <Outlet />
         </main>
       </SidebarInset>
